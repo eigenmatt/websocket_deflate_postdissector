42521c9c068cba4f836af63e0a3681fd2f35ada0
Matthew Chapman <eigenmatt@users.noreply.github.com>
2017-07-24 22:03:32 +1000

    Keep track of connections and packets processed: packets need to be digested sequentially for the zlib history buffer to be correct. Show decompressed packet data in a new payload tab.

--- websocket_deflate.lua
+++ websocket_deflate.lua
@@ -7,29 +7,83 @@ local zlib=require 'zlib'
 zlibPrefix = "\x78\x01"
 
 tcp_stream = Field.new("tcp.stream")
-websocket_payld = Field.new("websocket.payload")
+websocket_payload = Field.new("websocket.payload")
+websocket_rsv = Field.new("websocket.rsv")
 
-socketio_proto = Proto("socketio", "WebSocket permessage-deflate postdissector")
-type_F = ProtoField.string("socketio.type", "Text")
-socketio_proto.fields = {type_F}
+websocket_pmd_proto = Proto("websocket_pmd", "WebSocket permessage-deflate postdissector")
+websocket_pmd_payload_f = ProtoField.bytes("websocket_pmd.payload", "Inflated payload")
+websocket_pmd_proto.fields = {websocket_pmd_payload_f}
 
 local streams
+local frame_data
 
-function socketio_proto.init()
+function websocket_pmd_proto.init()
   streams = {}
+  frame_data = {}
 end
 
--- create a function to "postdissect" each frame
-function socketio_proto.dissector(buffer, pinfo, tree)
-    -- obtain the current values the protocol fields
-    local websocket_payload = websocket_payld()
-    if websocket_payload then
-	local data = zlibPrefix..websocket_payload.range:bytes():raw()
-	local inflateStream = zlib.inflate()
-	local inflated = inflateStream(data)
-        local subtree = tree:add(socketio_proto, "Inflated payload")
-        subtree:add(type_F, inflated)
+-- function to "postdissect" each frame
+function websocket_pmd_proto.dissector(buffer, pinfo, tree)
+    -- if we've already processed this frame, return stored data
+    -- decompression is stateful so we don't want to process again
+    local stored_payloads = frame_data[pinfo.number]
+    if stored_payloads ~= nil then
+        for i,b in ipairs(stored_payloads) do
+            local tvb = ByteArray.tvb(b, "Inflated payload")
+            tree:add(websocket_pmd_payload_f, tvb:range())
+        end
+        return
     end
+
+    -- check for WebSocket payloads in this frame
+    local websocket_payloads = { websocket_payload() }
+    if #websocket_payloads == 0 then
+        return
+    end
+
+    -- look up zlib state for this TCP stream
+    local stream = tcp_stream()()
+    local streamData = streams[stream]
+    if (streamData == nil) then
+        -- new TCP stream; initialize zlib state
+        local direction1InflateStream = zlib.inflate()
+        direction1InflateStream(zlibPrefix)
+        local direction2InflateStream = zlib.inflate()
+        direction2InflateStream(zlibPrefix)
+        streamData = { direction1InflateStream=direction1InflateStream,
+                       direction2InflateStream=direction2InflateStream,
+                       direction1Src=pinfo.src,
+                       direction1SrcPort=pinfo.src_port }
+        streams[stream] = streamData
+    end
+
+    -- determine direction (client-server or server-client)
+    local inflateStream;
+    if ((pinfo.src == streamData.direction1Src) and
+            (pinfo.src_port == streamData.direction1SrcPort)) then
+        inflateStream = streamData.direction1InflateStream
+    else
+        inflateStream = streamData.direction2InflateStream
+    end
+
+    -- process all Websocket payloads
+    websocket_pmd_payloads = {}
+    for i,payload in ipairs(websocket_payloads) do
+        local data = payload.range:bytes():raw().."\0\0\xff\xff"
+        local inflated = inflateStream(data)
+        local b = ByteArray.new()
+        b:set_size(#inflated)
+        for i=1,#inflated do
+            b:set_index(i-1,inflated:byte(i))
+        end
+        local tvb = ByteArray.tvb(b, "Inflated payload")
+        tree:add(websocket_pmd_payload_f, tvb:range())
+        table.insert(websocket_pmd_payloads, b)
+    end
+
+    -- save decompressed payloads for future invocations
+    -- decompression is stateful so we don't want to process again
+    frame_data[pinfo.number] = websocket_pmd_payloads
 end
 
-register_postdissector(socketio_proto)
+register_postdissector(websocket_pmd_proto)
